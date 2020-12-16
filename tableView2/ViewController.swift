d9bacc44e579ed53edfdfd4362f5204470258a31
Anna <annycvasiuc@yahoo.com>
2020-12-16 12:34:43 +0100

    Initial Commit

---
+++ tableView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView2
+//
+//  Created by Anna Cvasniuc on 16/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
